ebc30ddc9606557856b2ee0f05d08532f27cbd04
tobyluu <14794206+tobyluu@users.noreply.github.com>
2022-08-17 15:17:17 -0700

    Create magic8.py

---
+++ magic8.py
@@ -0,0 +1,46 @@
+# The Magic 8-Ball is a popular toy developed in the 1950s for fortune-telling or advice seeking.
+
+# Write a magic8.py Python program that can answer any “Yes” or “No” question with a different fortune each time it executes.
+
+import random
+
+name = "John"
+
+question = "Will I find my passion for life?"
+
+answer = ""
+
+random_number = random.randint(1,12)
+#print(random_number)
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+elif random_number == 10:
+  answer = "You're doomed. Just kidding"
+elif random_number == 11:
+  answer = "You will find Jesus."
+elif random_number == 12:
+  answer = "Jesus will find you."
+else:
+  answer = "Error"
+
+if name == "":
+  print("Question: " + question)
+else: print(name + " asks: " + question)
+print("Magic 8-Ball's answer: " + answer)
